d18b196e3749e165e407f93e1c1d957a5c76818e
Naser Odeh <naser@josequal.net>
2025-09-01 15:52:47 +0200

    Clean repo using .gitignore rules

--- wp-content/themes/theme/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($) {
-    $('p').each(function() {
-        if ($(this).text().trim() === '' || $(this).html().trim() === '&nbsp;') {
-            $(this).hide();
-        }
-    });
-});
